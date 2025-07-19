73b30bd96509c9c13e037963c857aaf2599053f5
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-19 11:22:01 +0700

    linked list

---
+++ src/linked list/linked list.ts
@@ -0,0 +1,71 @@
+/* data structures: Linked List */
+
+export class ListNode {
+    constructor(public val: number = 0, public next: ListNode | null = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+export function createSLList(arr: number[]): ListNode | null {
+    const [head, _] = createSLListWithTail(arr);
+
+    return head;
+}
+
+export function createSLListWithTail(arr: number[]): [ListNode | null, ListNode | null] {
+    if (arr === null || arr.length === 0)
+        return [null, null];
+
+    const head: ListNode = new ListNode(arr[0]);
+    let tail: ListNode = head;
+
+    for (let i = 1; i < arr.length; ++i) {
+        tail.next = new ListNode(arr[i]);
+        tail = tail.next;
+    }
+
+    return [head, tail];
+}
+
+export function printSLList(head: ListNode | null): void {
+    if (!head)
+        console.log("empty");
+    else
+        console.log(convertToArray(head).join(" "));
+}
+
+export function convertToArray(head: ListNode | null): number[] {
+    if (!head) return [];
+
+    const arr: number[] = [];
+    let cur: ListNode = head;
+
+    while (cur) {
+        arr.push(cur.val);
+        cur = cur.next;
+    }
+
+    return arr;
+}
+
+export function distanceFromHead(head: ListNode | null, node: ListNode | null): number {
+    if (!head || !node)
+        throw new Error("invalid arguments: head & node must be non-null");
+
+    let cur: ListNode = head;
+    let i = 0;
+
+    while (cur) {
+        if (cur === node)
+            break;
+
+        cur = cur.next;
+        ++i;
+    }
+
+    if (!cur)
+        throw new Error("invalid arguments: node might come before head or they are not on the same list");
+
+    return i;
+}

f2082e7554ee8354f2636324994318543b85b6f3
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-21 22:23:25 +0700

    data structures add: 145s, 226s modify: 94s, 144s, binary tree - binary tree.ts: + add method to convert Binary Tree to array + wrap createBinaryTree() and convertToArray() as static methods in class

---
+++ src/tree/226s.ts
@@ -0,0 +1,52 @@
+/* 226. invert binary tree */
+// #: tree + BFS + recursion
+import { Trees, TreeNode } from "./binary tree";
+
+class src226 {
+    // 1. iterative BFS
+    static invertTree(root: TreeNode | null): TreeNode | null {
+        const queue: (TreeNode | null)[] = [root];
+        let i = 0;
+
+        while (i < queue.length) {
+            const cur = queue[i++];
+
+            if (cur) {
+                [cur.left, cur.right] = [cur.right, cur.left];
+                queue.push(cur.left);
+                queue.push(cur.right);
+            }
+        }
+
+        return root;
+    }
+
+    // 2. recursive DFS
+    static invertTree2(root: TreeNode | null): TreeNode | null {
+        if (!root) return null;
+
+        [root.left, root.right] = [root.right, root.left];
+        this.invertTree2(root.left);
+        this.invertTree2(root.right);
+
+        return root;
+    }
+
+    // 3. iterative DFS
+
+    public static main(): void {
+        // add binary tree
+        const root: TreeNode | null = Trees.createBinaryTree(
+            [1, 2, 3, 4, 5, 6, 7]
+        );
+
+        // invert the binary tree
+        let ans1: TreeNode | null = src226.invertTree(root);
+        console.log(Trees.convertToArray(ans1).join(" "));
+
+        let ans2: TreeNode | null = src226.invertTree2(root);
+        console.log(Trees.convertToArray(ans2).join(" "));
+    }
+}
+
+src226.main();
